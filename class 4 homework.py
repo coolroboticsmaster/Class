5fe4619d4315d02c9b46f3c5a9d43c3e81e35ef0
coolroboticsmaster <amoghsinghal2008@gmail.com>
2018-12-22 10:00:51 +0530

    uploading files

--- class 4 homework.py
+++ class 4 homework.py
@@ -1,10 +1,6 @@
-
-
-
-#import datetime
-#import relativedelta   
-#tday=datetime.date.today()
-#wday=datetime.date(2014,6,14)
-#t_day=relativedelta(tday,wday)
-#print(t_day.years)
-#print(t_day.months)
+# import datetime
+# import relativedelta   
+# tday=datetime.date.today()
+# wday=datetime.date(2014,6,14)
+# t_day=relativedelta(tday,wday)
+# print(t_day.years)
